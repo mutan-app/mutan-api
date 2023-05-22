4b49cdb323ab24133d0038c91400a928af06d168
u120022 <u120022@st.pu-toyama.ac.jp>
2023-05-23 08:16:56 +0900

    :tada: import repository

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#[tokio::main]
+async fn main() {
+    let port: u16 = std::env::var("FUNCTIONS_CUSTOMHANDLER_PORT")
+        .ok()
+        .and_then(|text| text.parse().ok())
+        .unwrap_or(3000);
+
+    let addr = std::net::SocketAddrV4::new(std::net::Ipv4Addr::UNSPECIFIED, port);
+
+    use warp::Filter;
+    let version = warp::path!("version")
+        .map(|| format!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION")));
+
+    warp::serve(version).run(addr).await;
+}
